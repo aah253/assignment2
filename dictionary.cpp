47b78b55100d56fa096b1f7edca6b2a1ade6b2f3
aah253 <70031484+aah253@users.noreply.github.com>
2020-09-02 08:47:30 -0500

    Add files via upload

---
+++ dictionary.cpp
@@ -0,0 +1,26 @@
+#include <string>
+
+int min_word_length(string words[]) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
+
+int max_word_length(string words[]) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
+
+int word_length_range(string words[]) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
+
+int average_word_length(string words[]) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
+
+int most_common_word_length(string words[]) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
---
+++ dictionary.h
@@ -0,0 +1,25 @@
+
+int min_word_length(string words[]) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
+
+int max_word_length(string words[]) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
+
+int word_length_range(string words[]) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
+
+int average_word_length(string words[]) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
+
+int most_common_word_length(string words[]) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
