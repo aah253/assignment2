8308fcfc040ac2a939610a9267b3f4db029da578
aah253 <70031484+aah253@users.noreply.github.com>
2020-09-02 08:55:02 -0500

    Add files via upload

---
+++ searcher.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+int LinearSearch(int[], int, int);
+int BinarySearch(int[], int, int);
+
+
+int LinearSearch(int numbers[], int length, int n) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
+
+int binary_search(int numbers[], int length, int n) {
+    // TODO implement this function
+    throw "Unsupported Operation";
+}
